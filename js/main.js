64e221796fd4b7690335f38f8fd93197526d37d9
Russell Pate <russ@russpate.com>
2016-02-18 09:23:09 -0500

    adding style to the repo feed

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ var fullRepoData = "";
 _.each(repoData, function(item){
 
   fullRepoData +=
-  "<div class='repoData'><div class='repoStackLeft'><div class='repoInfo'><div class='repoTitleWrap'><div class='repoTitle'><a href ='" + item.clone_url + "'>" + item.name + "</a></div></div>" + "<div class='repoDescriptionWrap'><div class='repoDescription'>" + item.description + "</div></div>" + "<div class='repoTimeWrap'><div class='repoTime'>" + "updated" + moment.utc(item.created_at).fromNow() + "</div></div>" + "</div></div>" + "<div class='repoStackRight'><div class='stats'>" + "<ul>" + "<li>" + item.language + "</li>" + "<li><span class='octicon octicon-star'>" + item.stargazers_count + "</span></li>" + "<li><span class='octicon octicon-git-branch'></span>" + item.forks_count + "</span></li>" + "</ul>" + "</div></div></div>";
+  "<div class='repoData'><div class='repoStackLeft'><div class='repoInfo'><div class='repoTitleWrap'><div class='repoTitle'><a href ='" + item.clone_url + "'>" + item.name + "</a></div></div>" + "<div class='repoDescriptionWrap'><div class='repoDescription'>" + item.description + "</div></div>" + "<div class='repoTimeWrap'><div class='repoTime'>" + "updated" + moment.utc(item.created_at).fromNow() + "</div></div>" + "</div></div>" + "<div class='repoStackRight'><div class='stats'>" + "<ul>" + "<li>" + item.language + "</li>" + "<li><span class='octicon octicon-star'></span>" + item.stargazers_count + "</li>" + "<li><span class='octicon octicon-git-branch'></span>" + item.forks_count + "</span></li>" + "</ul>" + "</div></div></div>";
 });
 $('.repoDataWrap').append(fullRepoData);
 
