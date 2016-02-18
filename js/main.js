8edb494d32d0b1fcd797be24ad6920d60d94eb2d
Russell Pate <russ@russpate.com>
2016-02-17 23:44:47 -0500

    making progress on pulling in data with underscore and jquery for repository data

--- js/main.js
+++ js/main.js
@@ -1,11 +1,40 @@
-
+//
+//
+//
+///////// User Data
+//
+//
+//
 $('.avatar').append("<img src='" + userData.avatar_url + "'/>");
-
 $('.userName').text(userData.name);
 $('.atName').text(userData.login);
-$('.userEmail').append("<a href='mailto:#'>" + userData.email + "</a>");
-$('.userWebsite').append("<a href='#'>" +userData.blog + "</a>");
-// $('.userWebsite').text(userData.created_at);
+$('.userEmail').append("<span class='octicon octicon-mail'></span> " + "<a href='mailto:#'>" + userData.email + "</a>");
+$('.userWebsite').append("<span class='octicon octicon-link'></span> " + "<a href='#'>" +userData.blog + "</a>");
+$('.userJoin').append("<span class='octicon octicon-clock'></span> " + "Joined on " + moment.utc(userData.created_at).format("MMM Do, YYYY"));
+//
+//
+//
+///////// Repo Data
+//
+//
+//
+var fullRepoData = "";
+_.each(repoData, function(item){
+  fullRepoData +=
+  "<div class='repoData'><div class='repoInfo'><div class='repoTitleWrap'><div class='repoTitle'>" + item.name + "</div></div>" + "<div class='repoDescriptionWrap'><div class='repoDescription'>" + item.description + "</div></div>" + "<div class='repoTimeWrap'><div class='repoTime'>" + "updated" + moment.utc(item.created_at).fromNow() + "</div></div>" + "</div></div>";
 
 
-$('.userJoin').text("Joined on " + moment.utc(userData.created_at).format("MMM Do, YYYY"));
+});
+$('.repoDataWrap').append(fullRepoData);
+
+
+// "<div class='repoData'><div class='repoInfo'><div class='repoTitleWrap'><div class='repoTitle'>"
+// + item.name + "</div></div>"
+//
+// <div class='repoStatsWrap'>
+// <div class='repoStats'>
+// stats
+// </div>
+// </div>
+// </div>
+// </div>
